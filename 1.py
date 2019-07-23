85f2537a247176ff425c6517958faea6930ca3c7
damanraaj <40737621+damanraaj@users.noreply.github.com>
2019-07-22 22:11:46 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,14 @@
+
 def read(*argv):
-	for i in argv:
-		i=input()
-
+  for i in argv:
+    i[0]=input()
+	
 def write(*argv):
-	print(" ".join(map(str,argv)))
+  for i in argv:
+    print(i[0])  
 
-a,b,c=0
+a=[0]
+b=[0]
+c=[0]
 read(a,b,c)
 write(a,b,c)
